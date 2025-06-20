77a60d1f1de9c73a7b0a02a82fb20c85037b3b3b
Sunnyka <71286632+Sunnyka98@users.noreply.github.com>
2025-06-20 14:27:21 +0200

    Add some things for the new test

--- generate.php
+++ generate.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/kirby/bootstrap.php';
 
 $kirby = kirby();
-$staticSiteGenerator = new D4L\StaticSiteGenerator($kirby);
+$staticSiteGenerator = new JR\StaticSiteGenerator($kirby);
 $files = $staticSiteGenerator->generate('./build');
 
 // For debugging we want to display which pages were rendered
